4eb485d11bf7d7e58342f102349364bf6a423797
edward32tnt <edward32tnt@gmail.com>
2017-05-24 15:20:23 +0800

    [FIX] add Titler

---
+++ components/Titler.js
@@ -0,0 +1,11 @@
+import { Grid } from 'semantic-ui-react'
+
+export default () => (
+  <Grid columns={1}>
+    <Grid.Row>
+      <Grid.Column>
+        <p>My Blog</p>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+)
