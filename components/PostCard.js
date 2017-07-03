9b578d05f3bee6c76b4a21b8a137c0dbf2381003
edward32tnt <edward32tnt@gmail.com>
2017-07-03 18:19:25 +0800

    [FIX] fix css

--- components/PostCard.js
+++ components/PostCard.js
@@ -6,8 +6,15 @@ import { Card } from 'semantic-ui-react'
 export default (props) => {
   return (
     <Card link onClick={() => Router.push(`/post?id=${props.id}`, `/p/${props.id}`)}>
+      <style jsx global>{`
+        .card-title {
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+      `}</style>
       <Card.Content>
-        <Card.Header>{props.title}</Card.Header>
+        <Card.Header className={'card-title'}>{props.title}</Card.Header>
         <Card.Description>
           {props.desc}
         </Card.Description>
